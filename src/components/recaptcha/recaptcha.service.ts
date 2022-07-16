8d26941068e60657e840b73ec7ce74cc277380aa
Pablo Apausa Chamorro <pablo@MBP-de-Pablo.home>
2022-07-17 01:44:26 +0200

    Hardcode URL

--- src/components/recaptcha/recaptcha.service.ts
+++ src/components/recaptcha/recaptcha.service.ts
@@ -2,10 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 
 export default async function postRecaptcha(token: string) {
   try {
-    const { NEXT_PUBLIC_DOMAIN } = process.env;
-    const url: string = `${NEXT_PUBLIC_DOMAIN}/api/recaptcha`;
-
-    const { data: { data } }: AxiosResponse = await axios.post(url, { token });
+    const { data: { data } }: AxiosResponse = await axios.post('https://uzh.vercel.app/api/recaptcha', { token });
     return data;
   } catch (error) {
     return error;
