7e45c276f76ecc22623e62a35cdebd3a543222b2
Pablo Apausa Chamorro <pablo@MBP-de-Pablo.home>
2022-07-19 12:26:46 +0200

    Debug

--- src/lib/utils/localStorage.ts
+++ src/lib/utils/localStorage.ts
@@ -1,4 +1,4 @@
-const setItem = (name: string, data: any): void => localStorage.setItem(name, JSON.stringify(data));
+const setItem = (name: string, data: any): void => localStorage.setItem(name, data);
 const getItem = (name: string): any => localStorage.getItem(name);
 
 export { setItem, getItem };
