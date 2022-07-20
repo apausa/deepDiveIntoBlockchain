6136e2a693155dcd12f4af1d2da59e272eb9067d
Pablo Apausa Chamorro <pablo@MBP-de-Pablo.home>
2022-07-20 12:34:22 +0200

    Create Mint component
    Create verify component
    Create IAccount interface
    Create isHuman and isMinted state variables
    Create Mint component service
    Style

---
+++ src/lib/utils/associateNft.js
@@ -0,0 +1,30 @@
+const {
+  AccountId,
+  PrivateKey,
+  Client,
+  TokenAssociateTransaction,
+  TokenId,
+} = require('@hashgraph/sdk');
+
+const ACCOUNT_ID = AccountId.fromString(process.env.ACCOUNT_ID);
+const PRIVATE_KEY = PrivateKey.fromString(process.env.PRIVATE_KEY);
+const CLIENT_ID = AccountId.fromString(process.env.CLIENT_ID);
+const CLIENT_KEY = PrivateKey.fromString(process.env.CLIENT_KEY);
+const TOKEN_ID = TokenId.fromString('0.0.47705282');
+
+const client = Client.forTestnet().setOperator(ACCOUNT_ID, PRIVATE_KEY);
+
+async function associateNft() {
+  const associateClientTx = await new TokenAssociateTransaction()
+    .setAccountId(CLIENT_ID)
+    .setTokenIds([TOKEN_ID])
+    .freezeWith(client)
+    .sign(CLIENT_KEY);
+
+  const associateClientTxSubmit = await associateClientTx.execute(client);
+  const associateClientRx = await associateClientTxSubmit.getReceipt(client);
+
+  console.log(`- NFT association with Client's account: ${associateClientRx.status}\n`);
+}
+
+associateNft();
