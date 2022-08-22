d59db714ed6c42054720415fbe43ded1a490f7e5
Pablo Apausa Chamorro <pablo@MBP-de-Pablo.home>
2022-08-22 03:57:16 +0200

    Update routes

--- src/components/mint/mint.service.ts
+++ src/components/mint/mint.service.ts
@@ -1,20 +1,14 @@
 import axios from 'axios';
 import { IResponse } from '../../lib/types/services.types';
 
-// https://uzh.vercel.app/api/mint
-// http://localhost:3000/api/mint
-
 export async function mintNft(): Promise<boolean> {
-  const { data: { success } }: IResponse = await axios.get('http://localhost:3000/api/mint');
+  const { data: { success } }: IResponse = await axios.get('https://uzh.vercel.app/api/mint');
 
   return success;
 }
 
-// https://uzh.vercel.app/api/transfer
-// http://localhost:3000/api/transfer
-
 export async function transferNft(walletId: string): Promise<boolean> {
-  const { data: { success } }: IResponse = await axios.post('http://localhost:3000/api/transfer', { walletId });
+  const { data: { success } }: IResponse = await axios.post('https://uzh.vercel.app/api/transfer', { walletId });
 
   return success;
 }
